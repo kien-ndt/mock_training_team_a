9fa6932b89fed80d74303faa8b0ab49ac4ed678c
Kiên Nguyễn Trung <kien.nt.develop@gmail.com>
2022-01-27 17:22:34 +0700

    kiennt76: complete copyright component

---
+++ components/layout/footer/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+/**
+ * Content of Footer
+ *
+ * Param -> content: content of footer
+ *
+ * Example: <Footer content="@coppyright by KienNT76" />
+ */
+function Footer(props: any) {
+  // Get content from props
+  const content = props.content;
+
+  // CSS of Item content
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    fontSize: "20px",
+    minHeight: "40px",
+  }));
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={12}>
+          <Item>{content}</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 
 import UploadFile from "../components/common-components/uploadfile";
 import TitleUpload from "../components/modules/registration/common/title_upload";
+import Footer from "../components/layout/footer";
 
 const Home: NextPage = () => {
   const [state, setState] = useState("");
@@ -20,6 +21,7 @@ const Home: NextPage = () => {
       />
       <TitleUpload content="1. title dsadsadasdasdasds" />
       <TitleUpload content="Section A" color="red" fontWeight="bold" />
+      <Footer content="@coppyright by KienNT76" />
     </div>
   );
 };
