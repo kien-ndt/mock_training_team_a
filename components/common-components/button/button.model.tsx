3766e539169be7d13726cccd9705c74cd7a2b099
Kiên Nguyễn Trung <kien.nt.develop@gmail.com>
2022-01-27 18:05:23 +0700

    kiennt76: complete button next, back, search component

---
+++ components/common-components/button/button.model.tsx
@@ -0,0 +1,7 @@
+export type Props = {
+  size?: string;
+  name?: string;
+  onClick?: () => void;
+  padding?: string;
+  boderRadius?: string;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,17 @@ import React, { useEffect, useState } from "react";
 import UploadFile from "../components/common-components/uploadfile";
 import TitleUpload from "../components/modules/registration/common/title_upload";
 import Footer from "../components/layout/footer";
+import ButtonUI from "../components/common-components/button";
 
 const Home: NextPage = () => {
   const [state, setState] = useState("");
   useEffect(() => {
     console.log(state);
   }, [state]);
+
+  const testFunction = () => {
+    console.log("kiennt");
+  };
 
   return (
     <div>
@@ -22,6 +27,27 @@ const Home: NextPage = () => {
       <TitleUpload content="1. title dsadsadasdasdasds" />
       <TitleUpload content="Section A" color="red" fontWeight="bold" />
       <Footer content="@coppyright by KienNT76" />
+      <ButtonUI
+        name="next"
+        size="large"
+        padding="15px"
+        onClick={testFunction}
+        boderRadius="20px"
+      />
+      <ButtonUI
+        name="search"
+        size="large"
+        padding="15px"
+        onClick={testFunction}
+        boderRadius="20px"
+      />
+      <ButtonUI
+        name="back"
+        size="large"
+        padding="15px"
+        onClick={testFunction}
+        boderRadius="20px"
+      />
     </div>
   );
 };
