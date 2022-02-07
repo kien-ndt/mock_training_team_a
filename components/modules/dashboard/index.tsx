8fc87739d62db76b95969a696bf818cefeb16ca2
kienndt <nightstealth99@gmail.com>
2022-02-07 14:01:58 +0700

    dashboard v1

--- components/modules/dashboard/index.tsx
+++ components/modules/dashboard/index.tsx
@@ -1,11 +1,16 @@
 import Styles from "./dashboard.module.css"
 import PaginationButtons from "../../common-components/pagination"
+import Element from "./common/element"
 
 function DashboardComponent(){
     return(
         <div className={Styles["dashboard-container"]}>
             <div>search area</div>
-            <div style={{flex: 1}}>content area</div>
+            <div className={Styles["content"]}>
+                <Element 
+                    
+                />
+            </div>
             <div>
                 <PaginationButtons count={10} countPagesShow={3} page={1} showNearButton showLastButton/>
             </div>
